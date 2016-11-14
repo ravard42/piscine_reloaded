40130a85f9e243f8ec744eb08d7a59607d8de6fe
Robin AVARD <ravard@e3r6p6.42.fr>
2016-11-14 21:46:51 +0100

    modif

--- ex10/ft_swap.c
+++ ex10/ft_swap.c
@@ -4,5 +4,5 @@ void	ft_swap(int *a, int *b)
 
 	tmp = *a;
 	*a = *b;
-	*b = c;
+	*b = tmp;
 }
--- ex17/ft_strcmp.c
+++ ex17/ft_strcmp.c
@@ -4,7 +4,7 @@ int	ft_strcmp(char *s1, char *s2)
 	int	ret;
 
 	i = 0;
-	while(!(ret = ((unsigned char)s1[i] - (unsigned char)s2[i]))
+	while (!(ret = ((unsigned char)s1[i] - (unsigned char)s2[i]))
 		&& s1[i] && s2[i])
 		++i;
 	return (ret);
